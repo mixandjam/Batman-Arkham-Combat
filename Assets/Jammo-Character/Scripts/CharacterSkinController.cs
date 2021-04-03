b88c3dc197b39324ac3da57f33025e81f3514218
André Cardoso <amc.acardoso@gmail.com>
2021-04-03 22:26:53 +0200

    initial commit and setup

---
+++ Assets/Jammo-Character/Scripts/CharacterSkinController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSkinController : MonoBehaviour
+{
+    Animator animator;
+    Renderer[] characterMaterials;
+
+    public Texture2D[] albedoList;
+    [ColorUsage(true,true)]
+    public Color[] eyeColors;
+    public enum EyePosition { normal, happy, angry, dead}
+    public EyePosition eyeState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterMaterials = GetComponentsInChildren<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //ChangeMaterialSettings(0);
+            ChangeEyeOffset(EyePosition.normal);
+            ChangeAnimatorIdle("normal");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            //ChangeMaterialSettings(1);
+            ChangeEyeOffset(EyePosition.angry);
+            ChangeAnimatorIdle("angry");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            //ChangeMaterialSettings(2);
+            ChangeEyeOffset(EyePosition.happy);
+            ChangeAnimatorIdle("happy");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            //ChangeMaterialSettings(3);
+            ChangeEyeOffset(EyePosition.dead);
+            ChangeAnimatorIdle("dead");
+        }
+    }
+
+    void ChangeAnimatorIdle(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+
+    void ChangeMaterialSettings(int index)
+    {
+        for (int i = 0; i < characterMaterials.Length; i++)
+        {
+            if (characterMaterials[i].transform.CompareTag("PlayerEyes"))
+                characterMaterials[i].material.SetColor("_EmissionColor", eyeColors[index]);
+            else
+                characterMaterials[i].material.SetTexture("_MainTex",albedoList[index]);
+        }
+    }
+
+    void ChangeEyeOffset(EyePosition pos)
+    {
+        Vector2 offset = Vector2.zero;
+
+        switch (pos)
+        {
+            case EyePosition.normal:
+                offset = new Vector2(0, 0);
+                break;
+            case EyePosition.happy:
+                offset = new Vector2(.33f, 0);
+                break;
+            case EyePosition.angry:
+                offset = new Vector2(.66f, 0);
+                break;
+            case EyePosition.dead:
+                offset = new Vector2(.33f, .66f);
+                break;
+            default:
+                break;
+        }
+
+        for (int i = 0; i < characterMaterials.Length; i++)
+        {
+            if (characterMaterials[i].transform.CompareTag("PlayerEyes"))
+                characterMaterials[i].material.SetTextureOffset("_MainTex", offset);
+        }
+    }
+}
---
+++ Assets/Jammo-Character/Scripts/MovementInput.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementInput : MonoBehaviour
+{
+	private Animator anim;
+	private Camera cam;
+	private CharacterController controller;
+
+	private Vector3 desiredMoveDirection;
+	private Vector3 moveVector;
+
+	private float InputX;
+	private float InputZ;
+	private float verticalVel;
+
+	[Header("Settings")]
+	[SerializeField] float movementSpeed;
+	[SerializeField] float rotationSpeed = 0.1f;
+	[SerializeField] float fallSpeed = .2f;
+
+	[Header("Booleans")]
+	[SerializeField] bool blockRotationPlayer;
+	private bool isGrounded;
+
+
+	void Start()
+	{
+		anim = this.GetComponent<Animator>();
+		cam = Camera.main;
+		controller = this.GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		InputMagnitude();
+
+		isGrounded = controller.isGrounded;
+
+		if (isGrounded)
+			verticalVel -= 0;
+		else
+			verticalVel -= 1;
+
+		moveVector = new Vector3(0, verticalVel * fallSpeed * Time.deltaTime, 0);
+		controller.Move(moveVector);
+	}
+
+	void PlayerMoveAndRotation()
+	{
+		InputX = Input.GetAxis("Horizontal");
+		InputZ = Input.GetAxis("Vertical");
+
+		var camera = Camera.main;
+		var forward = cam.transform.forward;
+		var right = cam.transform.right;
+
+		forward.y = 0f;
+		right.y = 0f;
+
+		forward.Normalize();
+		right.Normalize();
+
+		desiredMoveDirection = forward * InputZ + right * InputX;
+
+		if (blockRotationPlayer == false)
+		{
+			//Camera
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+			controller.Move(desiredMoveDirection * Time.deltaTime * movementSpeed);
+		}
+		else
+		{
+			//Strafe
+			controller.Move((transform.forward * InputZ + transform.right * InputX) * Time.deltaTime * movementSpeed);
+		}
+	}
+
+	public void LookAt(Vector3 pos)
+	{
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(pos), rotationSpeed);
+	}
+
+	public void RotateToCamera(Transform t)
+	{
+		var forward = cam.transform.forward;
+
+		desiredMoveDirection = forward;
+		Quaternion lookAtRotation = Quaternion.LookRotation(desiredMoveDirection);
+		Quaternion lookAtRotationOnly_Y = Quaternion.Euler(transform.rotation.eulerAngles.x, lookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+		t.rotation = Quaternion.Slerp(transform.rotation, lookAtRotationOnly_Y, rotationSpeed);
+	}
+
+	void InputMagnitude()
+	{
+		//Calculate Input Vectors
+		InputX = Input.GetAxis("Horizontal");
+		InputZ = Input.GetAxis("Vertical");
+
+		//Calculate the Input Magnitude
+		float inputMagnitude = new Vector2(InputX, InputZ).sqrMagnitude;
+
+		//Physically move player
+		if (inputMagnitude > 0.1f)
+		{
+			anim.SetFloat("Blend", inputMagnitude, .1f, Time.deltaTime);
+			PlayerMoveAndRotation();
+		}
+		else
+		{
+			anim.SetFloat("Blend", inputMagnitude, .1f,Time.deltaTime);
+		}
+	}
+}
